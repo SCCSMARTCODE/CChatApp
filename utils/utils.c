e0193e3f33dea357a62c44115aba6ccfbeb102fc
SCCSMARTCODE <sccsmart247@gmail.com>
2025-01-04 18:34:51 +0100

    utils Updated

--- utils/utils.c
+++ utils/utils.c
@@ -5,72 +5,84 @@ int get_socket(){
     return socket(AF_INET, SOCK_STREAM, 0);
 }
 
-struct sockaddr *get_address(){
+struct sockaddr *get_address(int *ui_port, const char* ui_ip){
     char ip[IP_INPUT_MAX];
     char port_str[PORT_INPUT_MAX];
     int port;
 
+    // g_print("check this ---===>>0 [%d], [%s]", *ui_port, ui_ip);
 
-    printf("Input server IP Address: ");
+    if (!(ui_ip && ui_port)){
+        printf("Input server IP Address: ");
 
-    if (fgets(ip, IP_INPUT_MAX, stdin) != NULL){
-        size_t inputLen = strlen(ip);
+        if (fgets(ip, IP_INPUT_MAX, stdin) != NULL){
+            size_t inputLen = strlen(ip);
 
-        if ((inputLen > 0) && (ip[inputLen-1] == '\n')){
-            ip[inputLen-1] = '\0';
-        }
-        else if(inputLen == 0){
-            LOG_ERROR("IP Address buffer is empty\n");
-            return NULL;
-        }
-        else{
-            LOG_INFO("Please Input a correct IP Address\n");
-            return NULL;
-        }
-    }
-    else{
-        LOG_ERROR("Error reading IP Address");
-        return NULL;
-    }
-
-
-
-
-    printf("Input server PORT no: ");
-
-    if (fgets(port_str, PORT_INPUT_MAX, stdin) != NULL){
-        size_t inputLen = strlen(port_str);
-        char* endptr;
-
-        if ((inputLen > 0) && (port_str[inputLen-1] == '\n')){
-            port_str[inputLen-1] = '\0';
-
-            errno = 0;
-
-            port = (int)strtol(port_str, &endptr, 10);
-            if (*endptr != '\0' && *endptr != '\n'){
-                LOG_ERROR("Please Input a correct PORT no. eg [ 2000 ]\n");
+            if ((inputLen > 0) && (ip[inputLen-1] == '\n')){
+                ip[inputLen-1] = '\0';
+            }
+            else if(inputLen == 0){
+                LOG_ERROR("IP Address buffer is empty\n");
+                return NULL;
+            }
+            else{
+                LOG_INFO("Please Input a correct IP Address\n");
                 return NULL;
             }
         }
-        else if(inputLen == 0){
-            LOG_INFO("PORT buffer is empty.\n");
-        }
         else{
-            LOG_ERROR("Please Input a correct PORT no.\n");
+            LOG_ERROR("Error reading IP Address");
             return NULL;
         }
+
+
+
+
+        printf("Input server PORT no: ");
+
+        if (fgets(port_str, PORT_INPUT_MAX, stdin) != NULL){
+            size_t inputLen = strlen(port_str);
+            char* endptr;
+
+            if ((inputLen > 0) && (port_str[inputLen-1] == '\n')){
+                port_str[inputLen-1] = '\0';
+
+                errno = 0;
+
+                port = (int)strtol(port_str, &endptr, 10);
+                if (*endptr != '\0' && *endptr != '\n'){
+                    LOG_ERROR("Please Input a correct PORT no. eg [ 2000 ]\n");
+                    return NULL;
+                }
+            }
+            else if(inputLen == 0){
+                LOG_INFO("PORT buffer is empty.\n");
+            }
+            else{
+                LOG_ERROR("Please Input a correct PORT no.\n");
+                return NULL;
+            }
+        }
+        else{
+            LOG_ERROR("Error reading the PORT no..");
+            return NULL;
+        }
+
+        if (*ip)
+            LOG_INFO("Generating Address for IP [ %s ] and PORT [ %d ]...\n\n", ip, port);
+        else
+            LOG_INFO("Generating Address for IP [ 0.0.0.0 ] and PORT [ %d ]...\n\n", port);
     }
     else{
-        LOG_ERROR("Error reading the PORT no..");
-        return NULL;
+        size_t i;
+        for(i = 0; i < strlen(ui_ip); i++){
+            ip[i] = ui_ip[i];
+        }
+        ip[i] = '\0';
+        port = *ui_port;
     }
 
-    if (*ip)
-        LOG_INFO("Generating Address for IP [ %s ] and PORT [ %d ]...\n\n", ip, port);
-    else
-        LOG_INFO("Generating Address for IP [ 0.0.0.0 ] and PORT [ %d ]...\n\n", port);
-
+    // g_print("check this ---===>> [%s], [%d]", ip, port);
     struct sockaddr_in *new_address = malloc(sizeof(struct sockaddr_in));
     new_address->sin_port = htons(port);
     new_address->sin_family = AF_INET;
@@ -85,34 +97,41 @@ struct sockaddr *get_address(){
     return (struct sockaddr *)(new_address);
 }
 
-char *get_client_name(){
+char *get_client_name(const char* ui_client_name){
     char* clientName = malloc(sizeof(char) * CLIENT_NAME_INPUT_MAX);
 
-    printf("Input your USERNAME name (with %d max character): ", CLIENT_NAME_INPUT_MAX-2);
+    if (!ui_client_name){
 
-    if (fgets(clientName, CLIENT_NAME_INPUT_MAX, stdin) != NULL){
-        size_t inputLen = strlen(clientName);
-        // char* endptr;
+        printf("Input your USERNAME name (with %d max character): ", CLIENT_NAME_INPUT_MAX-2);
 
-        if ((inputLen > 0) && (clientName[inputLen-1] == '\n')){
-            clientName[inputLen-1] = '\0';
-        }
-        else if(inputLen == 0){
-            LOG_ERROR("USERNAME buffer is empty.\n");
-            free(clientName);
-            return NULL;
+        if (fgets(clientName, CLIENT_NAME_INPUT_MAX, stdin) != NULL){
+            size_t inputLen = strlen(clientName);
+            // char* endptr;
+
+            if ((inputLen > 0) && (clientName[inputLen-1] == '\n')){
+                clientName[inputLen-1] = '\0';
+            }
+            else if(inputLen == 0){
+                LOG_ERROR("USERNAME buffer is empty.\n");
+                free(clientName);
+                return NULL;
+            }
+            else{
+                printf("%ld\n", inputLen);
+                LOG_INFO("Please Input a valid name\n");
+                free(clientName);
+                return NULL;
+            }
         }
         else{
-            printf("%ld\n", inputLen);
-            LOG_INFO("Please Input a valid name\n");
+            LOG_ERROR("Error reading the USERNAME..");
             free(clientName);
             return NULL;
         }
     }
     else{
-        LOG_ERROR("Error reading the USERNAME..");
-        free(clientName);
-        return NULL;
+
+        strcat(clientName, ui_client_name);
     }
     return clientName;
 }
@@ -127,13 +146,13 @@ int setupClient(clientDetails *clientD){
         return -1;
     }
 
-    clientD->serverAddress = get_address();
+    clientD->serverAddress = get_address(NULL, NULL);
     if (clientD->serverAddress == NULL){
         LOG_ERROR("[ generating server address failed ]\n\n");
         return -1;
     }
 
-    clientD->clientName = get_client_name();
+    clientD->clientName = get_client_name(NULL);
     if (clientD->clientName == NULL){
         LOG_ERROR(" [ getting USERNAME failed ]\n\n");
         return -1;
@@ -141,7 +160,71 @@ int setupClient(clientDetails *clientD){
     return 0;
 }
 
+int setupClientFromUi(clientDetails *clientD, GtkBuilder* builder){
 
+    clientD->clientSocketFD = get_socket();
+    if (clientD->clientSocketFD == -1){
+        LOG_ERROR(" [ creating Client Socket Process Failed ]\n\n");
+        return -1;
+    }
+
+    GtkWidget* connection_dialog = GTK_WIDGET(gtk_builder_get_object(builder, "connection_dialog"));
+    GtkWidget* connection_dialog_button = GTK_WIDGET(gtk_builder_get_object(builder, "connection_dialog_button"));
+
+    
+
+    CDBHData *pack = (CDBHData *)malloc(sizeof(CDBHData));
+    pack->data = clientD;
+    pack->builder = builder;
+    pack->connection_dialog = connection_dialog;
+
+    g_signal_connect(connection_dialog_button, "clicked", G_CALLBACK(connection_dialog_button_handler), pack);
+
+    gint response = gtk_dialog_run(GTK_DIALOG(connection_dialog));
+    UNUSED(response);
+
+    return 0;
+}
+
+void connection_dialog_button_handler(GtkWidget* button, CDBHData *pack){
+
+    UNUSED(button);
+
+    GtkWidget* ip_entry;
+    GtkWidget* port_entry;
+    GtkWidget* user_name_entry;
+
+    ip_entry = GTK_WIDGET(gtk_builder_get_object(pack->builder, "connection_dialog_ip_entry"));
+    port_entry = GTK_WIDGET(gtk_builder_get_object(pack->builder, "connection_dialog_port_entry"));
+    user_name_entry = GTK_WIDGET(gtk_builder_get_object(pack->builder, "connection_dialog_username_entry"));
+
+    int port;
+
+    const char* ip = gtk_entry_get_text(GTK_ENTRY(ip_entry));
+    const char* port_str = gtk_entry_get_text(GTK_ENTRY(port_entry));
+    const char* username = gtk_entry_get_text(GTK_ENTRY(user_name_entry));
+
+    if (strlen(ip) < 1 || strlen(port_str) < 1 || strlen(username) < 1){
+        return;
+    }
+
+    char* endptr;
+    errno = 0;
+
+    port = (int)strtol(port_str, &endptr, 10);
+    if (*endptr != '\0' && *endptr != '\n'){
+        LOG_ERROR("Please Input a correct PORT no. eg [ 2000 ]\n");
+        return;
+    }
+
+    pack->data->serverAddress = get_address(&port, ip);
+    pack->data->clientName = get_client_name(username);
+
+
+    if (pack->data->clientName && pack->data->clientSocketFD && pack->data->serverAddress){
+        gtk_widget_hide(pack->connection_dialog);
+    }
+}
 
 int setupServer(serverDetails *serverD){
 
@@ -151,7 +234,7 @@ int setupServer(serverDetails *serverD){
         return -1;
     }
 
-    serverD->serverAddress = get_address();
+    serverD->serverAddress = get_address(NULL, NULL);
     if (serverD->serverAddress == NULL){
         LOG_ERROR(" [ generating server address failed ]\n\n");
         return -1;
@@ -162,6 +245,9 @@ int setupServer(serverDetails *serverD){
     }
     return 0;
 }
+
+
+
 
 void *handleOtherOperationsOnSeperateThread(void *serverD){
     int *client_fd = malloc(sizeof(int));
@@ -280,8 +366,47 @@ void *sendMessages(void *clientD_ptr) {
     return NULL;
 }
 
-void *receiveMessages(void *clientD_ptr) {
-    clientDetails *clientD = (clientDetails *)clientD_ptr;
+void *sendMessagesWithGUI(void *pack_ptr) {
+    SMData *pack = (SMData *)pack_ptr;
+    // char message[NETWORK_MESSAGE_BUFFER_SIZE];
+
+    while (1) {
+        
+        GtkWidget* send_button = GTK_WIDGET(gtk_builder_get_object(pack->builder, "send_button"));
+
+        SMHPack *smh_pack = malloc(sizeof(SMHPack));
+        smh_pack->data = pack->data;
+        smh_pack->builder = pack->builder;
+
+        g_signal_connect(send_button, "clicked", G_CALLBACK(send_message_handler), smh_pack);
+
+        if (!(smh_pack->status)) {
+            LOG_ERROR("Send failed");
+            break;
+        }
+    }
+
+    return NULL;
+}
+
+void send_message_handler(GtkWidget *button, SMHPack* pack){
+
+    UNUSED(button);
+
+    GtkWidget* message_entry = GTK_WIDGET(gtk_builder_get_object(pack->builder, "message_entry"));
+
+    const char* message = gtk_entry_get_text(GTK_ENTRY(message_entry));
+
+    if (strlen(message) >= 1){
+        if (send(pack->data->clientSocketFD, message, strlen(message), 0) == -1) {
+            pack->status = FALSE;
+        }
+    }
+
+}
+
+void *receiveMessages(void *pack_ptr) {
+    clientDetails *clientD = (clientDetails *)pack_ptr;
     char buffer[NETWORK_MESSAGE_BUFFER_SIZE];
     ssize_t bytesReceived;
 
@@ -297,6 +422,29 @@ void *receiveMessages(void *clientD_ptr) {
 
         buffer[bytesReceived] = '\0';
         printf("%s\n", buffer);
+    }
+
+    return NULL;
+}
+
+
+void *receiveMessagesWithGUI(void *clientD_ptr) {
+    clientDetails *clientD = (clientDetails *)clientD_ptr;
+    char buffer[NETWORK_MESSAGE_BUFFER_SIZE];
+    ssize_t bytesReceived;
+
+    while (1) {
+        bytesReceived = recv(clientD->clientSocketFD, buffer, sizeof(buffer) - 1, 0);
+        if (bytesReceived < 0) {
+            LOG_ERROR("Receive failed");
+            break;
+        } else if (bytesReceived == 0) {
+            LOG_INFO("Server disconnected.\n");
+            break;
+        }
+
+        buffer[bytesReceived] = '\0';
+        g_print("%s\n", buffer);
     }
 
     return NULL;
--- utils/utils.h
+++ utils/utils.h
@@ -9,18 +9,21 @@
 #include <unistd.h>
 #include <errno.h>
 #include <pthread.h>
+#include <gtk/gtk.h>
 
+#define UNUSED(x) (void)(x)
 
 #define MAX_CLIENTS 20
 #define IP_INPUT_MAX 40 // This extend to manage ipv4 and ipv6(for future implementation)
 #define PORT_INPUT_MAX 7
 #define CLIENT_NAME_INPUT_MAX 62
 #define NETWORK_MESSAGE_BUFFER_SIZE 200
+#define APP_UI_FILE_PATH "../gui/chat_app.glade"
 
 
-#define LOG_INFO(format, ...)  printf("[INFO]: " format "\n", ##__VA_ARGS__)
+#define LOG_INFO(format, ...)  g_print("[INFO]: " format "\n", ##__VA_ARGS__)
 #define LOG_ERROR(format, ...) fprintf(stderr, "[ERROR]: " format "\n", ##__VA_ARGS__)
-#define LOG_SUCCESS(format, ...) printf("[SUCCESS]: " format "\n", ##__VA_ARGS__)
+#define LOG_SUCCESS(format, ...) g_print("[SUCCESS]: " format "\n", ##__VA_ARGS__)
 #define MESSAGE_FORMAT "[USER: %s] Message: %s"
 
 
@@ -31,6 +34,26 @@ typedef struct clientDetails{
     char *clientName;
     struct sockaddr *serverAddress;
 }clientDetails;
+
+
+typedef struct CDBHData{
+    clientDetails* data;
+    GtkBuilder* builder;
+    GtkWidget* connection_dialog;
+}CDBHData;
+
+
+typedef struct SMData{
+    clientDetails* data;
+    GtkBuilder* builder;
+}SMData;
+
+
+typedef struct SMHPack{
+    clientDetails* data;
+    GtkBuilder* builder;
+    gboolean status;
+}SMHPack;
 
 
 
@@ -48,14 +71,23 @@ typedef struct HNAC{
 
 
 int setupClient(clientDetails *clientD);
+int setupClientFromUi(clientDetails *clientD, GtkBuilder* builder);
 int setupServer(serverDetails *serverD);
 void *handleOtherOperationsOnSeperateThread(void*);
 void *handleNewlyAcceptedClient(void *);
 void broadcastMessage(char *clientUsername, char *receivedMessage, int currentClientFD, int *clientFDStore);
 
 void *sendMessages(void *clientD_ptr);
+void *sendMessagesWithGUI(void *pack_ptr);
 void *receiveMessages(void *clientD_ptr);
+void *receiveMessagesWithGUI(void *clientD_ptr);
 
 int get_socket();
-struct sockaddr *get_address();
-char *get_client_name();
+// struct sockaddr *get_address();
+struct sockaddr *get_address(int *ui_port, const char* ui_ip);
+char *get_client_name(const char* ui_client_name);
+// char *get_client_name();
+
+
+void connection_dialog_button_handler(GtkWidget* button, CDBHData *pack);
+void send_message_handler(GtkWidget *button, SMHPack* pack);
