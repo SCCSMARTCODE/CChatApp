fe67f7dad2e67da0386e2b98556961403e7901cf
SCCSMARTCODE <sccsmart247@gmail.com>
2025-01-05 14:37:02 +0100

    integrated GUI in client socket

--- socket_client/main.c
+++ socket_client/main.c
@@ -20,19 +20,11 @@ int main(int argc, char **argv) {
     window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
     gtk_builder_connect_signals(builder, NULL);
     gtk_widget_show_all(window);
-    if (setupClientFromUi(&clientD, builder) == -1) {
-        // LOG_ERROR("Client setup failed.");
+    if (setupClientFromGUI(&clientD, builder) == -1) {
+        LOG_ERROR("Client setup failed.");
         return EXIT_FAILURE;
     }
 
-    if (connect(clientD.clientSocketFD, clientD.serverAddress, sizeof(*clientD.serverAddress)) < 0) {
-        LOG_ERROR("Failed to connect to server: %s", strerror(errno));
-        close(clientD.clientSocketFD);
-        free(clientD.clientName);
-        free(clientD.serverAddress);
-        return EXIT_FAILURE;
-    }
-    LOG_SUCCESS("Successfully connected to the server.");
 
      if (send(clientD.clientSocketFD, clientD.clientName, sizeof(clientD.clientName), 0) < 0){
         LOG_ERROR("Failed to send user details to server: %s", strerror(errno));
@@ -68,79 +60,18 @@ int main(int argc, char **argv) {
     }
     LOG_SUCCESS("Receive thread created successfully.");
 
+    gtk_main();
+
+    pthread_cancel(sendThread);
+    pthread_cancel(receiveThread);
     pthread_join(sendThread, NULL);
     pthread_join(receiveThread, NULL);
 
     LOG_INFO("Client shutting down...");
-    close(clientD.clientSocketFD);
-    free(clientD.clientName);
-    free(clientD.serverAddress);
+    cleanup(&clientD);
     LOG_SUCCESS("Client shutdown complete.");
     return EXIT_SUCCESS;
 
-    // ----------------------
     
-    gtk_main();
-
-    // ----------------------
-
-    
-    
-
-    // if (setupClient(&clientD) == -1) {
-    //     LOG_ERROR("Client setup failed.");
-    //     return EXIT_FAILURE;
-    // }
-
-    // if (connect(clientD.clientSocketFD, clientD.serverAddress, sizeof(*clientD.serverAddress)) < 0) {
-    //     LOG_ERROR("Failed to connect to server: %s", strerror(errno));
-    //     close(clientD.clientSocketFD);
-    //     free(clientD.clientName);
-    //     free(clientD.serverAddress);
-    //     return EXIT_FAILURE;
-    // }
-    // LOG_SUCCESS("Successfully connected to the server.");
-
-    // if (send(clientD.clientSocketFD, clientD.clientName, sizeof(clientD.clientName), 0) < 0){
-    //     LOG_ERROR("Failed to send user details to server: %s", strerror(errno));
-    //     close(clientD.clientSocketFD);
-    //     free(clientD.clientName);
-    //     free(clientD.serverAddress);
-    //     return EXIT_FAILURE;
-    // }
-    // LOG_SUCCESS("Successfully sent client details to the server.");
-
-    // pthread_t sendThread, receiveThread;
-
-    // if (pthread_create(&sendThread, NULL, sendMessages, &clientD) != 0) {
-    //     LOG_ERROR("Failed to create send thread: %s", strerror(errno));
-    //     close(clientD.clientSocketFD);
-    //     free(clientD.clientName);
-    //     free(clientD.serverAddress);
-    //     return EXIT_FAILURE;
-    // }
-    // LOG_SUCCESS("Send thread created successfully.");
-
-    // if (pthread_create(&receiveThread, NULL, receiveMessages, &clientD) != 0) {
-    //     LOG_ERROR("Failed to create receive thread: %s", strerror(errno));
-
-    //     pthread_cancel(sendThread);
-    //     pthread_join(sendThread, NULL);
-
-    //     close(clientD.clientSocketFD);
-    //     free(clientD.clientName);
-    //     free(clientD.serverAddress);
-    //     return EXIT_FAILURE;
-    // }
-    // LOG_SUCCESS("Receive thread created successfully.");
-
-    // pthread_join(sendThread, NULL);
-    // pthread_join(receiveThread, NULL);
-
-    // LOG_INFO("Client shutting down...");
-    // close(clientD.clientSocketFD);
-    // free(clientD.clientName);
-    // free(clientD.serverAddress);
-    // LOG_SUCCESS("Client shutdown complete.");
-    // return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
